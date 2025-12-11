f1118795ade6ea42397e7f0b2525ba498f2122de
kurizma <foriamkuri@gmail.com>
2025-12-11 10:53:44 +0200

    backend foundation

---
+++ backend/history-service/src/test/java/com/sass/sportsbet/history_service/HistoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sass.sportsbet.history_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
